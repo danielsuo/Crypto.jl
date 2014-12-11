58cf2eeadd09f6127991fd5461bfbcd7cd01cb99
Daniel Suo <danielsuo@gmail.com>
2014-12-11 17:08:34 -0500

    add openssl ecdsa

---
+++ deps/ecdsa.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h> // for NID_secp256k1
+
+// for executable:  gcc -lcrypto -std=c99 priv2pub.c -o priv2pub
+// for dynamic lib: gcc -lcrypto -std=c99 -fPIC -shared -Wl,-soname,libpriv2pub.so.1 priv2pub.c -o libpriv2pub.so.1.0
+
+// calculates and returns the public key associated with the given private key
+// - input private key and output public key are in hexadecimal
+// - output is null-terminated string
+// form = POINT_CONVERSION_[UNCOMPRESSED|COMPRESSED|HYBRID]
+void priv2pub( const unsigned char *priv_hex, 
+               point_conversion_form_t form,
+               unsigned char *ret)
+{
+
+  printf( "%s\n", priv_hex );
+
+  // create group
+  EC_GROUP *ecgrp = EC_GROUP_new_by_curve_name( NID_secp256k1 );
+
+  // convert priv key from hexadecimal to BIGNUM
+  BIGNUM *priv_bn = BN_new();
+  BN_hex2bn( &priv_bn, priv_hex );
+
+  // compute pub key from priv key and group
+  EC_POINT *pub = EC_POINT_new( ecgrp );
+  EC_POINT_mul( ecgrp, pub, priv_bn, NULL, NULL, NULL );
+
+  // convert pub_key from elliptic curve coordinate to hexadecimal string
+  memcpy(ret, EC_POINT_point2hex( ecgrp, pub, form, NULL ), 130 * sizeof(unsigned char));
+
+  EC_GROUP_free( ecgrp ); BN_free( priv_bn ); EC_POINT_free( pub );
+}
+
+int main( int argc, char *argv[] )
+{
+  // get priv key from cmd line and compute put key
+  unsigned char *a = malloc(130 * sizeof(unsigned char));
+  priv2pub( argv[1], POINT_CONVERSION_UNCOMPRESSED, a);
+
+  printf( "%s\n", a );
+
+  free( a );
+
+  return 0;
+}
+
+// testcase : 
+// $./priv2pub 18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725
+// 0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6
+// 0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6
--- src/Crypto.jl
+++ src/Crypto.jl
@@ -112,13 +112,61 @@ macro c(ret_type, func, arg_types, lib)
   end
 end
 
-@c Void RAND_poll () "libcrypto"
+@c Void RAND_poll () libcrypto
 # RAND_poll()
-@c Int RAND_load_file (ASCIIString, Int) "libcrypto"
+@c Int RAND_load_file (ASCIIString, Int) libcrypto
 # RAND_load_file("/dev/random", 32)
-@c Void RAND_bytes (Ptr{Uint8}, Int) "libcrypto"
+@c Void RAND_bytes (Ptr{Uint8}, Int) libcrypto
 # a = zeros(Uint8, 10)
 # RAND_bytes(a, 10)
-@c Void RAND_cleanup () "libcrypto"
+@c Void RAND_cleanup () libcrypto
 
+# @c Void gcry_randomize (Ptr{Uint8}, Int, Int) "libgcrypt"
+
+const NID_secp256k1 = 714
+typealias BIGNUM Void
+typealias BN_CTX Void
+typealias EC_POINT Void
+typealias EC_GROUP Void
+
+@c Ptr{EC_GROUP} EC_GROUP_new_by_curve_name (Int,) libcrypto
+ecgrp = EC_GROUP_new_by_curve_name(NID_secp256k1)
+
+@c Ptr{BIGNUM} BN_new () libcrypto
+priv_bn = BN_new()
+
+@c Ptr{BN_CTX} BN_CTX_new () libcrypto
+
+@c Ptr{EC_POINT} EC_POINT_new (Ptr{EC_GROUP},) libcrypto
+pub = EC_POINT_new(ecgrp)
+
+@c Int EC_POINT_mul (Ptr{EC_GROUP}, Ptr{EC_POINT}, Ptr{BIGNUM}, Ptr{EC_POINT}, Ptr{BIGNUM}, Ptr{BN_CTX}) libcrypto
+# EC_POINT_mul(ecgrp, pub, priv_bn, Nothing, Nothing, Nothing)
+
+@c Ptr{Uint8} EC_POINT_point2hex (Ptr{EC_GROUP}, Ptr{EC_POINT}, Int, Ptr{BN_CTX}) libcrypto
+EC_POINT_point2hex(ecgrp, pub, 4, BN_CTX_new())
+
+# @c Ptr{EVP_PKEY} EVP_PKEY_new () libcrypto
+# a = Array(Uint8, 32)
+# a = EVP_PKEY_new()
+
+@c Ptr{Uint8} priv2pub (ASCIIString, Int) libecdsa
+
+const libecdsa = joinpath(Pkg.dir(), "Crypto", "deps", "libecdsa.dylib")
+
+const POINT_CONVERSION_COMPRESSED = 2
+const POINT_CONVERSION_UNCOMPRESSED = 4
+# const POINT_CONVERSION_HYBRID = 6
+const COMPRESSED_LENGTH = 66
+const UNCOMPRESSED_LENGTH = 130
+
+
+a = zeros(Uint8, 130)
+ccall((:priv2pub, libecdsa), Void, (Ptr{Uint8}, Int, Ptr{Uint8}), "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725", 4, a)
+a = join([char(x) for x in a])
+
+hash = ""
+for i in 1:length(a)/2
+  hash = string(hash, char(parseint(a[i:i+1], 16)))
+end
 end
